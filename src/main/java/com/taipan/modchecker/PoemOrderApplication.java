1ef35962e84e65e47f8a042e2cc28bcfaf3ed287
bokovm <mbvlog07@gmail.com>
2025-12-09 17:19:53 +0300

    0.0.1

---
+++ src/main/java/com/taipan/modchecker/PoemOrderApplication.java
@@ -0,0 +1,13 @@
+package com.taipan.modchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoemOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoemOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taipan/modchecker/PoemOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taipan.modchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoemOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
